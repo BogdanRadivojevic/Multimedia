fba99ad861eca5b4e84872c1c840adc07180f78d
stefanmilovanovic02 <stefanmilovanovic989@gmail.com>
2024-10-21 09:14:15 +0200

    dodat hover efekat i povezani dugmici sa sekcijom

--- dugme.js
+++ dugme.js
@@ -2,3 +2,8 @@ function scrollToSection() {
     const section = document.getElementById("audio-usage");
     section.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToSectionn() {
+  const section = document.getElementById("multimedia-sections");
+  section.scrollIntoView({ behavior: "smooth" });
+}
